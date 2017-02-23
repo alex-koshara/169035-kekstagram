d96fce8b05dd1e2e1e09a82027e7764c04a25b65
alex-koshara <alex-koshara@mail.ru>
2017-02-23 22:09:23 +0300

    добавление сортировки

--- js/pictures.js
+++ js/pictures.js
@@ -3,13 +3,33 @@
 window.pictures = (function () {
   var DATA_URL = 'https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
   var pictures = [];
+  var currentPictures = [];
   var picturesContainer = document.querySelector('.pictures');
 
   window.load(DATA_URL, function (data) {
     pictures = data;
-    pictures.forEach(function (picture, index) {
-      picturesContainer.appendChild(window.pictureRender(picture, index));
+    currentPictures = data;
+    var sortFilters = document.querySelector('.filters');
+    var SHOW_FILTER_COUNT = 10;
+
+    sortFilters.addEventListener('click', function (e) {
+      switch (e.target.id) {
+        case 'filter-popular':
+          currentPictures = data;
+          break;
+        case 'filter-new':
+          currentPictures = getNewPictures(SHOW_FILTER_COUNT);
+          break;
+        case 'filter-discussed':
+          currentPictures = getDiscussedPictures(data);
+          break;
+      }
+      renderPictures(currentPictures);
     });
+
+    sortFilters.classList.remove('hidden');
+
+    renderPictures(pictures);
 
     picturesContainer.addEventListener('click', showPicture);
     picturesContainer.addEventListener('keydown', function (e) {
@@ -19,11 +39,40 @@ window.pictures = (function () {
     });
   });
 
+  function getDiscussedPictures(array) {
+    return array.slice(0).sort(function (item1, item2) {
+      return item1.comments.length - item2.comments.length;
+    });
+  }
+
+  function getNewPictures(count) {
+    var newPictures = [];
+    var dublicatePicutres = pictures.slice(0);
+    var item = null;
+    while (newPictures.length !== count) {
+      item = dublicatePicutres.splice(getRangomInteger(dublicatePicutres.length - 1), 1);
+      newPictures.push(item[0]);
+    }
+    return newPictures;
+  }
+
+  function getRangomInteger(maxInteger) {
+    return Math.round(Math.random() * maxInteger);
+  }
+
+  function renderPictures(array) {
+    picturesContainer.innerText = '';
+    currentPictures = array.slice(0);
+    array.forEach(function (picture, index) {
+      picturesContainer.appendChild(window.pictureRender(picture, index));
+    });
+  }
+
   function showPicture(event) {
     event.preventDefault();
     var pictureIndex = getPictureIndex(event);
     if (pictureIndex >= 0) {
-      window.showGallery(pictures[pictureIndex]);
+      window.showGallery(currentPictures[pictureIndex]);
     }
   }
 
@@ -33,7 +82,6 @@ window.pictures = (function () {
     if (picture.classList.contains('pictures')) {
       return -1;
     }
-
     while (!picture.classList.contains('picture')) {
       picture = picture.parentElement;
     }
